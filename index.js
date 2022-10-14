6d14ccedea7a64ba069c17d2c5a6a76cdf8f7ac2
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-10-14 17:48:48 +0100

    responsive layout second section

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+<nav className="navbar">
+        <div class="logo">
+            <img class="pangolin-icon" src="svg files/pangolin icon.svg" alt="">
+            
+            <a href="#">PANGOLIN</a>
+        </div>
+
+        
+           
+        <!-- <div> -->
+            <ul className="nav-list">
+                <!-- <li><a class="logo">Prime</a></li> -->
+                <li><a href="#Products">Products</a></li>
+                <li><a href="#Docs">Docs</a></li>
+                <li><a href="#Ecosystem">Ecosystem</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Contacts">Contacts</a></li>
+            </ul>
+        <!-- </div> -->
+    </nav>
+
+
+
 {/* <div class="bigbox2">
     <div class="innerbox2">
         <!-- <div class="svgbox2"><div class="pdfffosition"></div></div> -->
